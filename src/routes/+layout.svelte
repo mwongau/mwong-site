db53589e487c7568b5683f9cd8b5088b93ee98aa
M Wong <wongmt15@gmail.com>
2025-01-02 23:00:34 +1100

    02-01-24

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,21 +4,17 @@
 	
 	let { children } = $props();
 
-
-  //import { base } from "$app/paths";
   //import '$lib/styles/style.css'
   //import Header from '$lib/components/Header.svelte'
-  //import "../app.css";
 </script>
 
 
-<div class="nav" style="background-color: AliceBlue;">
+<div class="font-bold bg-blue-100">
 <a href="{base}/">Home</a> &nbsp; &nbsp;
 <a href="{base}/cv">CV</a> &nbsp; &nbsp;
+<a href="{base}/photos">Photos</a> &nbsp; &nbsp;
 <a href="{base}/privacy">Privacy</a>
 <br />
 </div>
-
-
 
 {@render children()}
---
+++ src/routes/photos/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+const images = ["/sv5site/acupy.jpg", "/sv5site/uts-bldg5.jpg", "/sv5site/hps.jpg"]
+const desc = [ "Australian Catholic University, Strathfield Campus, NSW, Australia",
+	          "University of Technology Sydney, Building 5",
+              "Homebush Public School, Homebush, NSW, Australia"]
+let count = $state(0)
+
+function next() {
+    count = (count + 1) % images.length
+}
+</script> 
+
+<button onclick={next} class="m-2 p-1 font-bold bg-blue-200">Next photo</button>
+<p class="my-2 font-bold">{desc[count]}</p>
+<img src="{images[count]}" alt="image" />
+
